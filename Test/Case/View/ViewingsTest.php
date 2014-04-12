55c3e47735661318b30aaf8312945a3d9a078244
Jaroslaw Zawila <jaroslaw.zawila@gmail.com>
2014-04-12 19:04:56 +0100

    added selenium test for cancel viewing

--- Test/Case/View/ViewingsTest.php
+++ Test/Case/View/ViewingsTest.php
@@ -89,12 +89,45 @@ class ViewingsTest extends PHPUnit_Extensions_Selenium2TestCase
         $this->byId('manage-34')->click();
         $this->assertEquals("New", $this->byId('ViewingStatus')->value());
 
-        $this->select($this->byId("ViewingStatus"))->selectOptionByValue('Closed');
+        $this->select($this->byId("ViewingStatus"))->selectOptionByValue('Updated');
         $this->byId("update-viewings")->click();
 
         $this->byId("menu-viewings")->click();
         $this->byId('manage-34')->click();
+        $this->assertEquals("Updated", $this->byId('ViewingStatus')->value());
+    }
+
+    public function testCancelledViewingCannotUpdateAnymore()
+    {
+        $this->url('http://127.0.1.1/');
+
+        $this->byId("login")->click();
+        $this->byId("username")->value('admin');
+        $this->byId("password")->value('admin');
+        $this->byId("login-button")->click();
+
+        $this->byId("menu-customer")->click();
+        $this->byId("menu-customer-view")->click();
+        $this->byId("view-2")->click();
+        $this->byId("viewings-tab")->click();
+        $this->byId("add-viewings")->click();
+        sleep(1);
+
+        $this->byId("ViewingComment")->value('comment');
+        $this->byId("submit-viewing")->click();
+
+        $this->byId("viewings-tab")->click();
+        $this->byId('manage-35')->click();
+        $this->assertEquals("New", $this->byId('ViewingStatus')->value());
+
+        $this->select($this->byId("ViewingStatus"))->selectOptionByValue('Closed');
+        $this->byId("update-viewings")->click();
+
+        $this->byId("menu-viewings")->click();
+        $this->byId('manage-35')->click();
         $this->assertEquals("Closed", $this->byId('ViewingStatus')->value());
 
+        $this->setExpectedException('PHPUnit_Extensions_Selenium2TestCase_WebDriverException');
+        $this->byId("update-viewings");
     }
 } 
