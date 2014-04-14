b1130dc7a547b4f53927ff34b724872cf296c8f3
Jaroslaw Zawila <jaroslaw.zawila@gmail.com>
2014-04-14 03:09:44 +0100

    added  requestdetails unit test

--- Test/Case/Controller/RequestdetailsControllerTest.php
+++ Test/Case/Controller/RequestdetailsControllerTest.php
@@ -13,48 +13,73 @@ class RequestdetailsControllerTest extends ControllerTestCase {
  * @var array
  */
 	public $fixtures = array(
-		'app.requestdetail',
-		'app.requests'
+		'app.requestdetails',
+		'app.request'
 	);
 
-/**
- * testIndex method
- *
- * @return void
- */
-	public function testIndex() {
+    public function testAddWithGetRequestRedirectToViewView() {
+        $this->generate('Requestdetails', array(
+            'components' => array(
+                'Auth')));
+
+        $data = array(
+            'Requestdetail' => array(
+                'id' => 1
+            )
+        );
+
+        $this->testAction('/requestdetails/add', array('method'=>'get', 'data' => $data));
+        $this->assertContains('http://127.0.1.1/admin/request/view', $this->headers['Location']);
+    }
+
+	public function testAddFailToSaveRedirectToViewAndDisplayMessage() {
+        $RD = $this->generate('Requestdetails', array(
+            'models' => array(
+                'Requestdetail' => array("save")),
+            'components' => array(
+                'Session' => array("setFlash"),
+                'Auth')));
+
+        $RD->Requestdetail
+            ->expects($this->once())
+            ->method('save')
+            ->will($this->returnValue(false));
+
+        $RD->Session
+            ->expects($this->once())
+            ->method('setFlash')
+            ->with("The requestdetail could not be saved. Please, try again.");
+
+        $data = array(
+            'Requestdetail' => array(
+                'id' => 1
+            )
+        );
+
+        $this->testAction('/requestdetails/add', array('method'=>'post', 'data' => $data));
+        $this->assertContains('http://127.0.1.1/admin/request/view', $this->headers['Location']);
 	}
 
-/**
- * testView method
- *
- * @return void
- */
-	public function testView() {
-	}
+    public function testAdd() {
+        $RD = $this->generate('Requestdetails', array(
+            'models' => array(
+                'Requestdetail' => array("save")),
+            'components' => array(
+                'Auth')));
 
-/**
- * testAdd method
- *
- * @return void
- */
-	public function testAdd() {
-	}
+        $RD->Requestdetail
+            ->expects($this->once())
+            ->method('save')
+            ->will($this->returnValue(true));
 
-/**
- * testEdit method
- *
- * @return void
- */
-	public function testEdit() {
-	}
+        $data = array(
+            'Requestdetail' => array(
+                'requests_id' => 1
+            )
+        );
 
-/**
- * testDelete method
- *
- * @return void
- */
-	public function testDelete() {
-	}
+        $this->testAction('/requestdetails/add', array('method'=>'post', 'data' => $data));
+        $this->assertContains('http://127.0.1.1/requests/view/1', $this->headers['Location']);
+    }
 
 }
